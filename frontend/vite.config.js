a3365799025e087eee3df38699bc0cb1ffe63076
jay <lyl.jay@qq.com>
2026-06-18 22:12:57 +0800

    config 调整前端配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // 前端开发服务器 5173, 把 /api 和 /ws 反向代理到后端 8080
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    global: 'window'
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
